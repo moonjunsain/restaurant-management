a9c39c28272ab479aad3eed61306c0e0d1ac2af1
moonjunsain <moonjunsain@gmail.com>
2023-07-29 14:06:46 -0700

    added event listener for form submittion on homepage

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,1 +1,24 @@
 const menuForm = document.getElementById('menuForm')
+
+menuForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const menuName = menuForm.menuName.value
+    const price = menuForm.price.value
+    const imgFile = menuForm.img.files[0]
+
+    const formData = new FormData()
+    formData.append('menuName', menuName)
+    formData.append('price', price)
+    formData.append('imgFile', imgFile)
+
+    fetch('/menus', {
+        method: 'POST',
+        body: formData
+    }).then((response) => {
+        return response.json
+    }).then(data => {
+        console.log('uploaded success', data)
+    }).catch(err => {
+        console.error("Error uploading", err)
+    })
+})
